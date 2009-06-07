a4fc8869421cc7eb94f14689ff4782145d9c8560
knue <knue@c3f3fedc-192b-0410-a103-a22bf914756c>
2009-06-07 22:38:44 +0000

    added library

---
+++ trunk/test/lib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_int(int n)
+{
+    printf("%i\n", n);
+}
+
+void print_float(float f)
+{
+    printf("%f\n", (float) f);
+}
+
+void print_double(double d)
+{
+    printf("%f\n", d);
+}
