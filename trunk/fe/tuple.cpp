1c2e68e9f0bf92f9e21f3148f60f59e981cd6245
knue <knue@c3f3fedc-192b-0410-a103-a22bf914756c>
2009-06-18 22:13:07 +0000

    added missing tuple.*

---
+++ trunk/fe/tuple.cpp
@@ -0,0 +1,135 @@
+/*
+ * Swift compiler framework
+ * Copyright (C) 2007-2009 Roland Leißa <r_leis01@math.uni-muenster.de>
+ *
+ * This framework is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this framework; see the file LICENSE. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "fe/tuple.h"
+
+#include "fe/access.h"
+#include "fe/expr.h"
+#include "fe/exprlist.h"
+
+namespace swift {
+
+/*
+ * constructor and destructor
+ */
+
+Tuple::Tuple(TypeNode* typeNode, Tuple* next, int line /*= NO_LINE*/)
+    : Node(line)
+    , typeNode_(typeNode)
+    , next_(next)
+{}
+
+Tuple::~Tuple()
+{
+    delete typeNode_;
+
+    if (next_)
+        delete next_;
+}
+
+/*
+ * virtual methods
+ */
+
+bool Tuple::analyze()
+{
+    bool result = true;
+
+    for (const Tuple* iter = this; iter != 0; iter = iter->next_)
+    {
+        Expr* expr = dynamic_cast<Expr*>(iter->typeNode_);
+        if (expr)
+            expr->neededAsLValue();
+
+        result &= iter->typeNode_->analyze();
+    }
+
+    return result;
+}
+
+/*
+ * further methods
+ */
+
+TypeList Tuple::getTypeList() const
+{
+    TypeList result;
+
+    for (const Tuple* iter = this; iter != 0; iter = iter->next_)
+        result.push_back( iter->typeNode_->getType() );
+
+    return result;
+}
+
+PlaceList Tuple::getPlaceList()
+{
+    PlaceList result;
+
+    for (Tuple* iter = this; iter != 0; iter = iter->next_)
+        result.push_back( iter->typeNode_->getPlace() );
+
+    return result;
+}
+
+const Tuple* Tuple::next() const
+{
+    return next_;
+}
+
+Tuple* Tuple::next()
+{
+    return next_;
+}
+
+const TypeNode* Tuple::typeNode() const
+{
+    return typeNode_;
+}
+
+TypeNode* Tuple::typeNode()
+{
+    return typeNode_;
+}
+
+std::string Tuple::toString() const
+{
+    std::string result;
+
+    for (const Tuple* iter = this; iter != 0; iter = iter->next_)
+        result += iter->typeNode_->toString() + ", ";
+
+    if ( !result.empty() )
+        result = result.substr( 0, result.size() - 2 );
+
+    return result;
+}
+
+bool Tuple::moreThanOne() const
+{
+    return next_;
+}
+
+void Tuple::emitStoreIfApplicable(Expr* expr)
+{
+    MemberAccess* ma = dynamic_cast<MemberAccess*>( typeNode() );
+    if (ma)
+        ma->emitStoreIfApplicable(expr);
+}
+
+} // namespace swift
